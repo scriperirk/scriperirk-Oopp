6b6ddb5b534698f2831df8d6e36a86d22481d7eb
Scriper.irk <scriper.irk@gmail.com>
2022-03-16 23:28:29 +0800

    one

---
+++ src/test/java/ru/netology/pervii/RadioTest.java
@@ -0,0 +1,14 @@
+package ru.netology.pervii;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
