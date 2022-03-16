8eb7af53e538c876bdaf8ca274be8c91a3211a52
Scriper.irk <scriper.irk@gmail.com>
2022-03-17 01:42:18 +0800

    commit_1

--- src/main/java/ru/netology/pervii/Radio.java
+++ src/main/java/ru/netology/pervii/Radio.java
@@ -1,4 +1,37 @@
 package ru.netology.pervii;
 
 public class Radio {
+
+    private int radioStation;
+
+    public int getRadioStation() {
+        return radioStation;
+    }
+
+    public void currentRadioStation() {
+        if (this.radioStation > this.radioStation + 1){
+            setRadioStation(this.radioStation + 1);
+        }
+        if (this.radioStation > this.radioStation - 1){
+            setRadioStation(this.radioStation - 1);
+        }
+    }
+
+    public void setRadioStation(int radioStation) {
+        if (radioStation < 10 && radioStation > 0) {
+            this.radioStation = radioStation;
+        }
+        else {
+            this.radioStation = 0;
+        }
+
+        //if (radioStation > 0) {
+        //   this.radioStation = radioStation;
+        //}
+        //else {
+        //    this.radioStation = 9;
+        // }
+    }
+
+
 }
--- src/test/java/ru/netology/pervii/RadioTest.java
+++ src/test/java/ru/netology/pervii/RadioTest.java
@@ -8,6 +8,15 @@ public class RadioTest {
 
     @Test
     public void test(){
+        Radio radioBox = new Radio();
+        radioBox.setRadioStation(0);
+
+        radioBox.currentRadioStation();
+
+        int expected = 15;
+        int actual = radioBox.getRadioStation();
+
+        assertEquals(expected,actual);
 
     }
 
