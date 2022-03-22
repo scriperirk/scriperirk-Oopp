7e6370ed0e6f8da78dc789ff0f25b5e342d57413
Scriper.irk <scriper.irk@gmail.com>
2022-03-23 00:42:38 +0800

    commit_10

--- src/main/java/ru/netology/pervii/Radio.java
+++ src/main/java/ru/netology/pervii/Radio.java
@@ -3,7 +3,7 @@ package ru.netology.pervii;
 public class Radio {
 
     private int radioStation;
-    private int radioVolume;
+    //private int radioVolume;
 
     //увеличение Станций
     public int getRadioStationNext() {
@@ -11,73 +11,73 @@ public class Radio {
     }
 
     public void increaseNext() {
-        setRadioStationNext(radioStation + 1);
+        setRadioStationNext(radioStation  + 1);
     }
 
-    public void setRadioStationNext(int newRadioStation) {
-        if (newRadioStation >= 0 && newRadioStation < 10) {
-            this.radioStation = newRadioStation;
+    public void setRadioStationNext(int radioStation) {
+        if (radioStation >= 0 && radioStation < 10) {
+            this.radioStation = radioStation;
             return;
         }
-        if (newRadioStation >= 0 && newRadioStation < 11) {
+        if (radioStation >= 0 && radioStation < 11) {
             this.radioStation = 0;
         }
     }
 
-    //уменьшение Станций
-    public int getRadioStationPrev() {
-        return radioStation;
-    }
-
-    public void increasePrev() {
-        setRadioStationPrev(radioStation - 1);
-    }
-
-    public void setRadioStationPrev(int newRadioStation) {
-        if (newRadioStation >= 0 && newRadioStation < 10) {
-            this.radioStation = newRadioStation;
-            return;
-        }
-        if (newRadioStation < 0) {
-            this.radioStation = 9;
-        }
-    }
-
-    //увеличение Громкости
-    public int getRadioVolumeNext() {
-        return radioVolume;
-    }
-
-    public void increaseVolumeNext() {
-        setRadioVolumeNext(radioVolume + 1);
-    }
-
-    public void setRadioVolumeNext(int newRadioVolume) {
-        if (newRadioVolume >= 0 && newRadioVolume < 11) {
-            this.radioVolume = newRadioVolume;
-            return;
-        }
-        if (newRadioVolume > 10) {
-            this.radioVolume = 10;
-        }
-    }
-
-    //уменьшение Громкости
-    public int getRadioVolumePrev() {
-        return radioVolume;
-    }
-
-    public void increaseVolumePrev() {
-        setRadioVolumePrev(radioVolume - 1);
-    }
-
-    public void setRadioVolumePrev(int newRadioVolume) {
-        if (newRadioVolume >= 0 && newRadioVolume < 11) {
-            this.radioVolume = newRadioVolume;
-            return;
-        }
-        if (newRadioVolume < 0) {
-            this.radioVolume = 0;
-        }
-    }
+//    //уменьшение Станций
+//    public int getRadioStationPrev() {
+//        return radioStation;
+//    }
+//
+//    public void increasePrev() {
+//        setRadioStationPrev(radioStation - 1);
+//    }
+//
+//    public void setRadioStationPrev(int newRadioStation) {
+//        if (newRadioStation >= 0 && newRadioStation < 10) {
+//            this.radioStation = newRadioStation;
+//            return;
+//        }
+//        if (newRadioStation < 0) {
+//            this.radioStation = 9;
+//        }
+//    }
+//
+//    //увеличение Громкости
+//    public int getRadioVolumeNext() {
+//        return radioVolume;
+//    }
+//
+//    public void increaseVolumeNext() {
+//        setRadioVolumeNext(radioVolume + 1);
+//    }
+//
+//    public void setRadioVolumeNext(int newRadioVolume) {
+//        if (newRadioVolume >= 0 && newRadioVolume < 11) {
+//            this.radioVolume = newRadioVolume;
+//            return;
+//        }
+//        if (newRadioVolume > 10) {
+//            this.radioVolume = 10;
+//        }
+//    }
+//
+//    //уменьшение Громкости
+//    public int getRadioVolumePrev() {
+//        return radioVolume;
+//    }
+//
+//    public void increaseVolumePrev() {
+//        setRadioVolumePrev(radioVolume - 1);
+//    }
+//
+//    public void setRadioVolumePrev(int newRadioVolume) {
+//        if (newRadioVolume >= 0 && newRadioVolume < 11) {
+//            this.radioVolume = newRadioVolume;
+//            return;
+//        }
+//        if (newRadioVolume < 0) {
+//            this.radioVolume = 0;
+//        }
+//    }
 }
