17d83a21c1330d854fc4622eefac0036a32d011d
Scriper.irk <scriper.irk@gmail.com>
2022-03-23 21:59:47 +0800

    commit_final

--- src/main/java/ru/netology/pervii/Radio.java
+++ src/main/java/ru/netology/pervii/Radio.java
@@ -2,8 +2,8 @@ package ru.netology.pervii;
 
 public class Radio {
 
-    private int radioStation;
-    //private int radioVolume;
+    public int radioStation;
+    public int radioVolume;
 
     //увеличение Станций
     public int getRadioStationNext() {
@@ -15,69 +15,69 @@ public class Radio {
     }
 
     public void setRadioStationNext(int radioStation) {
-        if (radioStation >= 0 && radioStation < 10) {
+         if (radioStation >= 0 && radioStation < 10) {
             this.radioStation = radioStation;
             return;
         }
-        if (radioStation >= 0 && radioStation < 11) {
+        if (radioStation >= 10) {
             this.radioStation = 0;
         }
     }
 
-//    //уменьшение Станций
-//    public int getRadioStationPrev() {
-//        return radioStation;
-//    }
-//
-//    public void increasePrev() {
-//        setRadioStationPrev(radioStation - 1);
-//    }
-//
-//    public void setRadioStationPrev(int newRadioStation) {
-//        if (newRadioStation >= 0 && newRadioStation < 10) {
-//            this.radioStation = newRadioStation;
-//            return;
-//        }
-//        if (newRadioStation < 0) {
-//            this.radioStation = 9;
-//        }
-//    }
-//
-//    //увеличение Громкости
-//    public int getRadioVolumeNext() {
-//        return radioVolume;
-//    }
-//
-//    public void increaseVolumeNext() {
-//        setRadioVolumeNext(radioVolume + 1);
-//    }
-//
-//    public void setRadioVolumeNext(int newRadioVolume) {
-//        if (newRadioVolume >= 0 && newRadioVolume < 11) {
-//            this.radioVolume = newRadioVolume;
-//            return;
-//        }
-//        if (newRadioVolume > 10) {
-//            this.radioVolume = 10;
-//        }
-//    }
-//
-//    //уменьшение Громкости
-//    public int getRadioVolumePrev() {
-//        return radioVolume;
-//    }
-//
-//    public void increaseVolumePrev() {
-//        setRadioVolumePrev(radioVolume - 1);
-//    }
-//
-//    public void setRadioVolumePrev(int newRadioVolume) {
-//        if (newRadioVolume >= 0 && newRadioVolume < 11) {
-//            this.radioVolume = newRadioVolume;
-//            return;
-//        }
-//        if (newRadioVolume < 0) {
-//            this.radioVolume = 0;
-//        }
-//    }
+    //уменьшение Станций
+    public int getRadioStationPrev() {
+        return radioStation;
+    }
+
+    public void increasePrev() {
+        setRadioStationPrev(radioStation - 1);
+    }
+
+    public void setRadioStationPrev(int newRadioStation) {
+        if (newRadioStation >= 0 && newRadioStation < 10) {
+            this.radioStation = newRadioStation;
+            return;
+        }
+        if (newRadioStation < 0) {
+            this.radioStation = 9;
+        }
+    }
+
+    //увеличение Громкости
+    public int getRadioVolumeNext() {
+        return radioVolume;
+    }
+
+    public void increaseVolumeNext() {
+        setRadioVolumeNext(radioVolume + 1);
+    }
+
+    public void setRadioVolumeNext(int newRadioVolume) {
+        if (newRadioVolume >= 0 && newRadioVolume < 11) {
+            this.radioVolume = newRadioVolume;
+            return;
+        }
+        if (newRadioVolume > 10) {
+            this.radioVolume = 10;
+        }
+    }
+
+    //уменьшение Громкости
+    public int getRadioVolumePrev() {
+        return radioVolume;
+    }
+
+    public void increaseVolumePrev() {
+        setRadioVolumePrev(radioVolume - 1);
+    }
+
+    public void setRadioVolumePrev(int newRadioVolume) {
+        if (newRadioVolume >= 0 && newRadioVolume < 11) {
+            this.radioVolume = newRadioVolume;
+            return;
+        }
+        if (newRadioVolume < 0) {
+            this.radioVolume = 0;
+        }
+    }
 }
--- src/test/java/ru/netology/pervii/RadioTest.java
+++ src/test/java/ru/netology/pervii/RadioTest.java
@@ -10,8 +10,8 @@ public class RadioTest {
     public void testNextOne() {
         Radio radioBox = new Radio();
 
-        radioBox.getRadioStationNext();
-        //radioBox.setRadioStationNext();
+        radioBox.radioStation = 0;
+
         radioBox.increaseNext();
 
         int expected = 1;
@@ -25,8 +25,8 @@ public class RadioTest {
     public void testNextTwo() {
         Radio radioBox = new Radio();
 
-        radioBox.getRadioStationNext();
-        //radioBox.setRadioStationNext(1);
+        radioBox.radioStation = 1;
+
         radioBox.increaseNext();
 
         int expected = 2;
@@ -36,200 +36,215 @@ public class RadioTest {
 
     }
 
-//    @Test
-//    public void testNextThree() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioStationNext(8);
-//        radioBox.increaseNext();
-//
-//        int expected = 9;
-//        int actual = radioBox.getRadioStationNext();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testNextFour() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioStationNext(9);
-//        radioBox.increaseNext();
-//
-//        int expected = 0;
-//        int actual = radioBox.getRadioStationNext();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testPrevOne() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioStationPrev(9);
-//        radioBox.increasePrev();
-//
-//        int expected = 8;
-//        int actual = radioBox.getRadioStationPrev();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testPrevTwo() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioStationPrev(8);
-//        radioBox.increasePrev();
-//
-//        int expected = 7;
-//        int actual = radioBox.getRadioStationPrev();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testPrevThree() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioStationPrev(1);
-//        radioBox.increasePrev();
-//
-//        int expected = 0;
-//        int actual = radioBox.getRadioStationPrev();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testPrevFour() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioStationPrev(0);
-//        radioBox.increasePrev();
-//
-//        int expected = 9;
-//        int actual = radioBox.getRadioStationPrev();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    //громкость
-//    @Test
-//    public void testVolumeNextOne() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioVolumeNext(10);
-//        radioBox.increaseVolumeNext();
-//
-//        int expected = 10;
-//        int actual = radioBox.getRadioVolumeNext();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testVolumeNextTwo() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioVolumeNext(9);
-//        radioBox.increaseVolumeNext();
-//
-//        int expected = 10;
-//        int actual = radioBox.getRadioVolumeNext();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testVolumeNextThree() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioVolumeNext(0);
-//        radioBox.increaseVolumeNext();
-//
-//        int expected = 1;
-//        int actual = radioBox.getRadioVolumeNext();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testVolumeNextFour() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioVolumeNext(1);
-//        radioBox.increaseVolumeNext();
-//
-//        int expected = 2;
-//        int actual = radioBox.getRadioVolumeNext();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testVolumePrevOne() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioVolumePrev(10);
-//        radioBox.increaseVolumePrev();
-//
-//        int expected = 9;
-//        int actual = radioBox.getRadioVolumePrev();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testVolumePrevTwo() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioVolumePrev(9);
-//        radioBox.increaseVolumePrev();
-//
-//        int expected = 8;
-//        int actual = radioBox.getRadioVolumePrev();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testVolumePrevThree() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioVolumePrev(1);
-//        radioBox.increaseVolumePrev();
-//
-//        int expected = 0;
-//        int actual = radioBox.getRadioVolumePrev();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void testVolumePrevFour() {
-//        Radio radioBox = new Radio();
-//
-//        //radioBox.setRadioVolumePrev(0);
-//        radioBox.increaseVolumePrev();
-//
-//        int expected = 0;
-//        int actual = radioBox.getRadioVolumePrev();
-//
-//        assertEquals(expected, actual);
-//
-//    }
+    @Test
+    public void testNextThree() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioStation = 8;
+
+        radioBox.increaseNext();
+
+        int expected = 9;
+        int actual = radioBox.getRadioStationNext();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testNextFour() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioStation = 9;
+
+        radioBox.increaseNext();
+
+        int expected = 0;
+        int actual = radioBox.getRadioStationNext();
+
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void testPrevOne() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioStation = 0;
+
+        radioBox.increasePrev();
+
+        int expected = 9;
+        int actual = radioBox.getRadioStationPrev();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testPrevTwo() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioStation = 1;
+
+        radioBox.increasePrev();
+
+        int expected = 0;
+        int actual = radioBox.getRadioStationPrev();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testPrevThree() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioStation = 9;
+
+        radioBox.increasePrev();
+
+        int expected = 8;
+        int actual = radioBox.getRadioStationPrev();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testPrevFour() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioStation = 8;
+
+        radioBox.increasePrev();
+
+        int expected = 7;
+        int actual = radioBox.getRadioStationPrev();
+
+        assertEquals(expected, actual);
+
+    }
+
+    //громкость
+    @Test
+    public void testVolumeNextOne() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioVolume = 0;
+
+        radioBox.increaseVolumeNext();
+
+        int expected = 1;
+        int actual = radioBox.getRadioVolumeNext();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testVolumeNextTwo() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioVolume = 1;
+
+        radioBox.increaseVolumeNext();
+
+        int expected = 2;
+        int actual = radioBox.getRadioVolumeNext();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testVolumeNextThree() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioVolume = 9;
+
+        radioBox.increaseVolumeNext();
+
+        int expected = 10;
+        int actual = radioBox.getRadioVolumeNext();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testVolumeNextFour() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioVolume = 10;
+
+        radioBox.increaseVolumeNext();
+
+        int expected = 10;
+        int actual = radioBox.getRadioVolumeNext();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testVolumePrevOne() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioVolume = 10;
+
+        radioBox.increaseVolumePrev();
+
+        int expected = 9;
+        int actual = radioBox.getRadioVolumePrev();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testVolumePrevTwo() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioVolume = 9;
+
+        radioBox.increaseVolumePrev();
+
+        int expected = 8;
+        int actual = radioBox.getRadioVolumePrev();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testVolumePrevThree() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioVolume = 1;
+
+        radioBox.increaseVolumePrev();
+
+        int expected = 0;
+        int actual = radioBox.getRadioVolumePrev();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testVolumePrevFour() {
+        Radio radioBox = new Radio();
+
+        radioBox.radioVolume = 0;
+
+        radioBox.increaseVolumePrev();
+
+        int expected = 0;
+        int actual = radioBox.getRadioVolumePrev();
+
+        assertEquals(expected, actual);
+
+    }
 }
